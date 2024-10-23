fe532f351f10d9ec9319df3c5156a87772e45423
anybody-books <anybody-books.0j@icloud.com>
2024-10-23 20:33:00 +0700

    Fixed issue with data serialization

---
+++ pinch.js
@@ -0,0 +1,1 @@
+const memoize = fn => { const cache = new Map(); return (...args) => { const key = JSON.stringify(args); if (cache.has(key)) return cacheget(key); const result = fn.apply(this, args); cache.set(key, result); return result; }; };
